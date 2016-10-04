6979b19401808e3d36a99266c22bded14dee082f
samwalker <samwalker505@gmail.com>
2016-10-04 23:35:40 +0800

    addedd option allow origin

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -45,6 +45,11 @@ class BaseHanler(webapp2.RequestHandler):
         except Exception as e:
             logging.debug(e)
 
+    def options(self):
+        self.response.headers['Access-Control-Allow-Origin'] = '*'
+        self.response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
+        self.response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE'
+
     def res(self, content='no content', status=200, content_type='application/json'):
         self.response.headers['Access-Control-Allow-Origin'] = '*'
         self.response.set_status(status)
