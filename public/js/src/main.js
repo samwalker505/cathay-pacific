18b787badba3e357eef17bb6460165e2759b2fcc
samwalker <samwalker505@gmail.com>
2016-10-23 11:47:50 +0800

    added jp template

--- public/js/src/main.js
+++ public/js/src/main.js
@@ -13,8 +13,9 @@ Vue.use(VueResource);
 
 const routes = [
   { path: '/test', component: Test },
-  { path: '/trips/:trip_id/TH', component: Thailand},
   { path: '/trips/:trip_id/JP', component: Japan},
+  { path: '/trips/:trip_id/Tokyo/NRT', redirect: '/trips/:trip_id/JP'},
+  { path: '/trips/:trip_id/TH', component: Thailand},
   { path: '/trips/:trip_id/Bangkok', redirect: '/trips/:trip_id/TH'},
   { path: '/trips/:trip_id',  component: Trip }
 ]
